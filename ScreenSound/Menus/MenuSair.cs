9b92bc5d2ddf069418c8edcafa2541052bbbd1e5
jvnakad <joaonakad@gmail.com>
2024-09-24 22:03:34 -0300

    Remoção Switch por Dictionary

---
+++ ScreenSound/Menus/MenuSair.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ScreenSound.Menus;
+
+internal class MenuSair : Menu
+{
+    internal void Executar()
+    {
+        Console.WriteLine("Até logo 0/");
+    }
+}
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -15,6 +15,14 @@ internal class Program
         bandasRegistradas.Add(Coldplay.Nome, Coldplay);
         bandasRegistradas.Add(beatles.Nome, beatles);
 
+        Dictionary<int, Menu> opcoes = new();
+        opcoes.Add(1, new MenuRegistrarBanda());
+        opcoes.Add(2, new MenuRegistrarAlbum());
+        opcoes.Add(3, new MenuMostrarBandas());
+        opcoes.Add(4, new MenuAvaliarBanda());
+        opcoes.Add(5, new MenuExibirDetalhes());
+        opcoes.Add(6, new MenuSair());
+
         void ExibirLogo()
         {
             Console.WriteLine(@"
@@ -43,39 +51,15 @@ internal class Program
             string opcaoEscolhida = Console.ReadLine()!;
             int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
 
-            switch (opcaoEscolhidaNumerica)
+            if (opcoes.ContainsKey(opcaoEscolhidaNumerica))
             {
-                case 1:
-                    MenuRegistrarBanda menu1 = new();
-                    menu1.Executar(bandasRegistradas);
-                    ExibirOpcoesDoMenu();
-                    break;
-                case 2:
-                    MenuRegistrarAlbum menu2 = new();
-                    menu2.Executar(bandasRegistradas);
-                    ExibirOpcoesDoMenu();
-                    break;
-                case 3:
-                    MenuMostrarBandas menu3 = new();
-                    menu3.Executar(bandasRegistradas);
-                    ExibirOpcoesDoMenu();
-                    break;
-                case 4:
-                    MenuAvaliarBanda menu4 = new();
-                    menu4.Executar(bandasRegistradas);
-                    ExibirOpcoesDoMenu();
-                    break;
-                case 5:
-                    MenuExibirDetalhes menu5 = new MenuExibirDetalhes();
-                    menu5.Executar(bandasRegistradas);
-                    ExibirOpcoesDoMenu();
-                    break;
-                case 0:
-                    Console.WriteLine("Até logo 0/");
-                    break;
-                default:
-                    Console.WriteLine("Opção inválida");
-                    break;
+                Menu menuASerExibido = opcoes[opcaoEscolhidaNumerica];
+                menuASerExibido.Executar(bandasRegistradas);
+                if(opcaoEscolhidaNumerica >= 0) ExibirOpcoesDoMenu();
+            } 
+            else
+            {
+                Console.WriteLine("Opção inválida");
             }
         }
         ExibirOpcoesDoMenu();
